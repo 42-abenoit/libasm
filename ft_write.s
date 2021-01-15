0775b97f2a397c8c579909fed0fe8f3ed9b83dcb
abenoit <abenoit@framagit.org>
2021-01-15 10:48:22 +0100

    write linux added and old src folder removal

--- ft_write.s
+++ ft_write.s
@@ -1,3 +1,5 @@
+%ifdef	os_mac
+
 extern	___error
 global	_ft_write
 
@@ -7,8 +9,8 @@ _ft_write:
 			mov		rsi, 0x0
 			mov	rax, 0x20000BD
 			syscall
-			cmp rax, 9
 			pop		rsi
+			cmp rax, 9
 			jz ret_error
 			mov	rax, 0x2000004
 			syscall
@@ -23,3 +25,35 @@ ret_error:
 			pop	rax
 			mov	rax, -1
 			ret
+
+%endif
+
+%ifdef os_linux
+
+extern	__errno_location
+global	ft_write
+
+section	.text
+ft_write:
+			push	rsi
+			mov		rsi, 0x0
+			mov	rax, 0x05
+			syscall
+			pop		rsi
+			cmp rax, 9
+			jz ret_error
+			mov	rax, 0x01
+			syscall
+			cmp	rax, 0
+			jle	_error
+			ret
+
+_error:
+			neg	rax
+			push	rax
+			call	__errno_location
+			pop		rax
+			mov	rax, -1
+			ret
+
+%endif
--- src_linux/ft_list_push_front_bonus.s
+++
@@ -1,8 +0,0 @@
-global	ft_list_push_front
-section	.text
-
-ft_list_push_front:
-					mov	r8,	[rsi]
-					mov	rsi, r8
-					mov rsi, rsi
-					ret
